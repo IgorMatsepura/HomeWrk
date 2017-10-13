39a97cdec8ccc072eca607f9551d690013c88aa9
IgorMatsepura <Igor.Matsepura@gmail.com>
2017-10-13 11:36:08 +0300

    Wrk p1 v3

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int trees,i, j,m ,n,z;
+    int trees=0,i, j,m ,n;
     printf("Input number : ");
     scanf ("%d",&trees);
     // Check for negative numbers and Christmas tree more than 2 characters
@@ -23,46 +23,36 @@ int main()
     }
     printf("\n");
     //Triangle in decreasing
-         for(n = 0; n <= trees; n++){
-         for(m = 0; m <= trees; m++){
-            if(n < m )
-            printf("*");
+    for(n = 0; n <= trees; n++){
+        for(m = 0; m <= trees; m++){
+        if(n < m )
+        printf("*");
         }
         printf("\n");
-        }
-        // crism tree
-        for (i = trees; i <= trees; i++) {
-        for (j = 0; j <= i; j++) {
-            if (j == i) {
-                for( z = 0; z <=trees - j - 1; z++) {
-                   printf(" ");
-                }
-                printf(" ");
-            } else {
-                for(z = 0; z <= trees - j; z++) {
-                    printf(" ");
-                }
-            }
-            for(z = 0; z <= 2 * j; z++) {
-                printf("*");
-            }
-            if (j == i) {
-                 printf(" ");
-            }
-           printf("\n");
-        }
-        // penek for trees
-        if (i=-1);
-         while (i<=trees)
-        //for (i=-1;i<=trees;i++)
-            {
-            if (i!=trees)
+    }
+
+    //cristmas tree created
+    int spaces = 0;
+    int stars = 0;
+    int k =0, j=0;
+    for (i = 0 , spaces = trees, stars = -1; i <= trees; i++, stars += 2, spaces-- ){
+        for (j =0; j < spaces; j++){
             printf(" ");
-            if (i==trees)
+        }
+
+        for (k = 0; k < stars; i++ ){
             printf("*");
-            i++;
-            }
         }
-        }
+
+        printf("\n");
+    }
+
+    // create penek
+    for (i = 0; i < trees - 1; i++){
+        printf(" ");
+    }
+
+    printf("*\n");
+    }
    return 0;
 }
